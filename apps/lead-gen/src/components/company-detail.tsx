fb30f6bcfdbb0f53e1d2335b2a02dee8a438aed6
Vadim Nicolai <nicolai.vadim@gmail.com>
2026-04-15 17:34:12 +0300

    (lead-gen): 4,200/4,521 (93%), 219 candidates. ~320 left. Under 5 minutes.

--- apps/lead-gen/src/components/company-detail.tsx
+++ apps/lead-gen/src/components/company-detail.tsx
@@ -1360,11 +1360,6 @@ export function CompanyDetail({ companyKey, companyId }: Props) {
           <TabNav.Link active>Overview</TabNav.Link>
           {isAdmin && (
             <TabNav.Link asChild>
-              <Link href={`/companies/${effectiveKey}/contacts`}>Contacts</Link>
-            </TabNav.Link>
-          )}
-          {isAdmin && (
-            <TabNav.Link asChild>
               <Link href={`/companies/${effectiveKey}/emails`}>Emails</Link>
             </TabNav.Link>
           )}
